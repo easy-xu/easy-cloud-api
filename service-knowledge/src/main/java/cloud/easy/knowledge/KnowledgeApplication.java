d14c89ebceb883a35329a4da9992099de80f5b34
xuhonglin <xuhonglin@sinosoft.com.cn>
2021-11-20 15:35:37 +0800

    启动类

--- service-knowledge/src/main/java/cloud/easy/knowledge/KnowledgeApplication.java
+++ service-knowledge/src/main/java/cloud/easy/knowledge/KnowledgeApplication.java
@@ -1,4 +1,4 @@
-package cloud.easy;
+package cloud.easy.knowledge;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
@@ -6,21 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Title: QuestionnaireApplication
+ * Title: Application
  * Description:
  *
  * @author Xu Honglin
  * @version 1.0
- * @date 2021/8/30 21:02 首次创建
- * @date 2021/8/30 21:02 最后修改
  */
 @Slf4j
 @SpringBootApplication
 @MapperScan("cloud.easy.*.mapper")
-public class QuestionnaireApplication {
-
+public class KnowledgeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(QuestionnaireApplication.class, args);
+        SpringApplication.run(KnowledgeApplication.class, args);
         log.info("-----问卷服务启动成功-----");
     }
 }
