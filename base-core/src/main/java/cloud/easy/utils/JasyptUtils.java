180fc99d2e9366da074df1995bccb0420a657477
xuhonglin <xuhonglin@sinosoft.com.cn>
2021-11-23 23:14:50 +0800

    配置文件加密

---
+++ base-core/src/main/java/cloud/easy/utils/JasyptUtils.java
@@ -0,0 +1,57 @@
+package cloud.easy.utils;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEByteEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+
+/**
+ * @author xuhonglin
+ */
+public class JasyptUtils {
+
+    private JasyptUtils(){
+
+    }
+
+    /**
+     * Jasypt生成加密结果
+     *
+     * @param password 配置文件中设定的加密密码 jasypt.encryptor.password
+     * @param value    待加密值
+     * @return 加密后密文
+     */
+    public static String encryptPwd(String password, String value) {
+        PooledPBEStringEncryptor encryptOr = new PooledPBEStringEncryptor();
+        encryptOr.setConfig(cryptOr(password));
+        return encryptOr.encrypt(value);
+    }
+
+    /**
+     * 解密
+     *
+     * @param password 配置文件中设定的加密密码 jasypt.encryptor.password
+     * @param value    待解密密文
+     * @return 解密后明文
+     */
+    public static String decryptPwd(String password, String value) {
+        PooledPBEStringEncryptor encryptOr = new PooledPBEStringEncryptor();
+        encryptOr.setConfig(cryptOr(password));
+        return encryptOr.decrypt(value);
+    }
+
+    /**
+     * @param password salt
+     * @return 加密配置
+     */
+    private static SimpleStringPBEConfig cryptOr(String password) {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm(StandardPBEByteEncryptor.DEFAULT_ALGORITHM);
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName(null);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        return config;
+    }
+}
