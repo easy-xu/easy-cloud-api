dde70eaa9b51d210ab09e39c5bbb3660fcdec7cb
xuhonglin <xuhonglin@sinosoft.com.cn>
2021-11-24 14:18:48 +0800

    配置文件加密

--- base-core/src/main/java/cloud/easy/utils/JasyptUtils.java
+++ base-core/src/main/java/cloud/easy/utils/JasyptUtils.java
@@ -43,10 +43,16 @@ public class JasyptUtils {
      * @param password jasypt密码
      * @return 加密配置
      */
-    private static SimpleStringPBEConfig config(String password) {
+    public static SimpleStringPBEConfig config(String password) {
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPoolSize(1);
         config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
         return config;
     }
 }
--- service-cms/src/main/java/cloud/easy/Application.java
+++ service-cms/src/main/java/cloud/easy/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  */
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cloud.easy")
 @MapperScan("cloud.easy.*.mapper")
 public class Application {
     public static void main(String[] args) {
