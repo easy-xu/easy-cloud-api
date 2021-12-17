f2f1a2582eeb623d84b16f3a7bc8889dc1ddbcf2
xuhonglin <xuhonglin@sinosoft.com.cn>
2021-12-17 21:16:07 +0800

    批处理启动类

---
+++ service-job/src/main/java/cloud/easy/job/Application.java
@@ -0,0 +1,22 @@
+package cloud.easy.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author xu honglin
+ * @date 2021/12/7 11:34
+ */
+@Slf4j
+@SpringBootApplication(scanBasePackages = "cloud.easy")
+@MapperScan("cloud.easy.*.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("-----批处理服务启动成功-----");
+    }
+}
